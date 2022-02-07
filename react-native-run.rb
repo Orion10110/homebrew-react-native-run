af14b98a93cfd7ec01530d8609668be80281ac70
Artur Kalach <artur_kalach@epam.com>
2022-02-07 17:37:48 +0200

    feat(homebrew) react native script

---
+++ react-native-run.rb
@@ -0,0 +1,11 @@
+class ReactNativeRun < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/Orion10110/react-native-run/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "f2d33bb144cedd068431d128ba287b4a1880e3e31d0005c98e97cba06b5041a1"
+  license ""
+
+  def install
+    bin.install "rn_script"
+  end
+end
