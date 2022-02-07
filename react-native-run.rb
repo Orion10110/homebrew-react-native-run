918c59dd90716d12c219709e517cc98c912b2473
Artur Kalach <artur_kalach@epam.com>
2022-02-07 19:45:12 +0200

    fix(brew): update script

--- react-native-run.rb
+++ react-native-run.rb
@@ -1,8 +1,8 @@
 class ReactNativeRun < Formula
   desc ""
   homepage ""
-  url "https://github.com/Orion10110/react-native-run/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "03af4348c23efe060e1d8bb5efaa520803b1a0d961cfceec6f89228d47014c7a"
+  url "https://github.com/Orion10110/react-native-run/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "3fb23df5bd8192c43a7234a9893e6c5ee9fb727d732b5a44b1e6199e146d5e8d"
   license "MIT"
 
   def install
