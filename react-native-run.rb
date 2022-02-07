7c0597b3a751906015bde7af589224714ed286d8
Artur Kalach <artur_kalach@epam.com>
2022-02-07 19:38:12 +0200

    feat(rn): update license

--- react-native-run.rb
+++ react-native-run.rb
@@ -3,7 +3,7 @@ class ReactNativeRun < Formula
   homepage ""
   url "https://github.com/Orion10110/react-native-run/archive/refs/tags/v0.1.2.tar.gz"
   sha256 "03af4348c23efe060e1d8bb5efaa520803b1a0d961cfceec6f89228d47014c7a"
-  license ""
+  license "MIT"
 
   def install
     bin.install "rn_consts"
